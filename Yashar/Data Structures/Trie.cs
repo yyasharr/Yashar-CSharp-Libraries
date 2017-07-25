756326bcead35ef5cf6d5637d6d9321a4b66b1f0
yyasharr <yashar_gharabaghian@yahoo.com>
2017-07-25 02:30:40 -0700

    Pushing Trie and TrieNode.

---
+++ Yashar/Data Structures/Trie.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yashar
+{
+    namespace DataStructures
+    {
+        public class Trie //For solving Q208 from Leetcode
+        {
+            TrieNode root;
+
+            /// <summary>
+            /// Creates a new Trie data structure and creates an empty TrieNode as root which doens't hold any character.
+            /// </summary>
+            public Trie()
+            {
+                root = new TrieNode();
+            }
+
+            /// <summary>
+            /// Inserts a word into the trie.
+            /// </summary>
+            /// <param name="word"></param>
+            public void Insert(string word)
+            {
+                TrieNode[] children = root.Children;
+                for (int i = 0; i < word.Length; i++)
+                {
+                    int ch = word[i] - 'a';
+                    if (children[ch] != null) //if we have seen it before
+                    {
+                        if (i == word.Length - 1)
+                            children[ch].IsLeaf = true;
+                    }
+                    //if we have not seen this at this level.
+                    else
+                    {
+                        children[ch] = new TrieNode(word[i]);
+                        if (i == word.Length - 1)//last char
+                        {
+                            children[ch].IsLeaf = true;
+                        }
+                    }
+                    children = children[ch].Children;
+                }
+            }
+
+            /// <summary>
+            /// Retruns true if the given word is found in the trie, false otherwise.
+            /// </summary>
+            /// <param name="word"></param>
+            /// <returns></returns>
+            public bool Search(string word)
+            {
+                TrieNode[] children = root.Children;
+
+                for (int i = 0; i < word.Length; i++)
+                {
+                    int ch = word[i] - 'a';
+                    if (children[ch] == null)
+                        return false;
+                    else
+                    {
+                        if (i == word.Length - 1 && children[ch].IsLeaf == true)//last character
+                            return true;
+
+                        children = children[ch].Children;
+                    }
+                }
+                return false;
+            }
+
+            /// <summary>
+            /// Generates and returns the list of the suggestions, which are the words that can be created with the given prefix.
+            /// </summary>
+            /// <param name="prefix"></param>
+            /// <returns></returns>
+            public List<string> Suggestions(string prefix)
+            {
+                TrieNode current_node = getCurrentNode(prefix);
+                List<string> result = new List<string>();
+
+                if (current_node == null) //means we couldn't find the prefix in the trienode
+                    return result;
+
+                GenerateSuggestions(prefix, current_node, result);
+
+                return result;
+
+            }
+
+            private void GenerateSuggestions(string prefix, TrieNode current_node, List<string> result)
+            {
+                for (int i = 0; i < 26; i++)
+                {
+                    if (current_node.Children[i] != null) //means we can continue from here
+                    {
+                        char ch = Convert.ToChar(i + 'a');
+                        string new_prefix = prefix + ch;
+                        if (current_node.Children[i].IsLeaf == true)
+                        {
+                            result.Add(new_prefix);
+                        }
+                        GenerateSuggestions(new_prefix, current_node.Children[i], result);
+                    }
+                }
+            }
+
+            private TrieNode getCurrentNode(string prefix)
+            {
+                TrieNode curr = root;
+
+                for (int i = 0; i < prefix.Length; i++)
+                {
+                    int ch = prefix[i] - 'a';
+                    if (curr.Children[ch] != null)
+                    {
+                        curr = curr.Children[ch];
+                        if (i == prefix.Length - 1)//last char of prefix found!
+                        {
+                            return curr;
+                        }
+                    }
+                    else
+                        return null;
+                }
+                return null;
+            }
+
+            /// <summary>
+            /// Returns true if there is any word in the trie that starts with the given prefix, false otherwise.
+            /// </summary>
+            /// <param name="prefix"></param>
+            /// <returns></returns>
+            public bool StartsWith(string prefix)
+            {
+                TrieNode[] children = root.Children;
+                for (int i = 0; i < prefix.Length; i++)
+                {
+                    int ch = prefix[i] - 'a';
+                    if (children[ch] == null)
+                        return false;
+                    else
+                    {
+                        if (i == prefix.Length - 1)
+                            return true;
+                    }
+                    children = children[ch].Children;
+                }
+                return false;
+            }
+        }
+    }
+}
---
+++ Yashar/Data Structures/TrieNode.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yashar
+{
+    namespace DataStructures
+    {
+        public class TrieNode
+        {
+            char _c;
+            TrieNode[] _children;
+            bool _isLeaf;
+
+            /// <summary>
+            /// Creates a new TrieNode which doesn't have any character and only used as the root of the trie.
+            /// </summary>
+            public TrieNode()
+            {
+                _children = new TrieNode[26];
+            }
+
+            /// <summary>
+            /// Creates a new TrieNode and assigns the character ch to the node.
+            /// </summary>
+            /// <param name="ch"></param>
+            public TrieNode(char ch)
+            {
+                _c = ch;
+                _children = new TrieNode[26];
+            }
+
+            /// <summary>
+            /// Gets the character assigned to this node.
+            /// </summary>
+            public char C
+            {
+                get { return _c; }
+            }
+
+            /// <summary>
+            /// Gets the list of the children mappings for this node.
+            /// </summary>
+            public TrieNode[] Children
+            {
+                get { return _children; }
+            }
+            
+            /// <summary>
+            /// Return true if this current node (character) ends up to a complete word.
+            /// </summary>
+            public bool IsLeaf
+            {
+                get { return _isLeaf; }
+                set { _isLeaf = value; }
+            }
+
+            
+        }
+    }
+}
