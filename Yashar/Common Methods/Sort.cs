0e48252587fabe220670afa028b76bca996f6c4e
Alireza <Alireza_paridar@hotmail.com>
2017-08-08 21:24:51 +0430

    Check Null Condition. Add Async Method.

--- Yashar/Common Methods/Sort.cs
+++ Yashar/Common Methods/Sort.cs
@@ -16,6 +16,11 @@ namespace Yashar
             /// <param name="array"></param>
             public static void MergeSort(this int[] array)
             {
+                if (array == null)
+                {
+                    return;
+                }
+
                 int n = array.Length;
                 if (n < 2) return;
                 int mid = n / 2;
@@ -35,6 +40,22 @@ namespace Yashar
                 MergeSort(left);
                 Merge(left, right, array);
             }
+
+            /// <summary>
+            /// Async Sorts a given array of integers, using MergeSort algorithm. Also can be used as int[].MergeSort();
+            /// </summary>
+            /// <param name="array"></param>
+            /// <returns></returns>
+            public static Task<int[]> MergeSortAsync(int[] array)
+            {
+                int[] temp = array;
+                return Task.Factory.StartNew(() =>
+                {
+                    MergeSort(temp);
+                    return temp;
+                });
+            }
+
             private static void Merge(int[] left, int[] right, int[] array)
             {
                 int i = 0; int j = 0; int k = 0;
@@ -77,8 +98,31 @@ namespace Yashar
             /// <param name="array"></param>
             public static void QuickSort(this int[] array)
             {
+                if(array == null)
+                {
+                    return;
+                }
+
                 QuickSort(array, 0, array.Length - 1);
             }
+
+            public static Task<int[]> QuickSortAsync(this int[] array)
+            {
+                int[] temp = array;
+
+                return Task.Factory.StartNew(() =>
+                {
+                    if (temp == null)
+                    {
+                        return null;
+                    }
+
+                    QuickSort(temp, 0, temp.Length - 1);
+
+                    return temp;
+                });
+            }
+
             private static void QuickSort(int[] array, int min, int max)
             {
                 int index = Partition(array, min, max);
@@ -87,6 +131,7 @@ namespace Yashar
                 if (max > index)
                     QuickSort(array, index + 1, max);
             }
+
             private static int Partition(int[] array, int min, int max)
             {
                 int pivot = array[(min + max) / 2];
